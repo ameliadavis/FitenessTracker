55cdfff78cd20233b2c6a48fbb8b82bec4801825
Amelia Davis <ameliaaltman22@gmail.com>
2020-04-20 22:12:36 -0600

    corrected Typos in server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //if deployed use the deployed database. Otherwise use the local mongo headlines databs
-var MONGODB_URI = process.end.MONGODB_URI || "mongo://localhost/workout"
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
 
 //connect to the Mongo DB
 mongoose.connect(MONGODB_URI);
