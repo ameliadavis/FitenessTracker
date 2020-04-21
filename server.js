a269b845e8a48c8cf8ac985acc6c099f4a9ef1ef
Amelia Davis <ameliaaltman22@gmail.com>
2020-04-20 22:08:05 -0600

    updated server file to push onto heroku

--- server.js
+++ server.js
@@ -14,10 +14,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+//if deployed use the deployed database. Otherwise use the local mongo headlines databs
+var MONGODB_URI = process.end.MONGODB_URI || "mongo://localhost/workout"
+
+//connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api-routes.js"));
